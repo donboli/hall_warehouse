85172ff80d1cfda7f2457d86cb992515b0404ca2
Juan Becker <wolli.becker@gmail.com>
2016-05-11 00:23:21 -0500

    handle ResourcesForm submit event

--- imports/ui/ResourceForm.jsx
+++ imports/ui/ResourceForm.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import Constants from '../lib/constants.js';
+import Resources from '../api/resources.js';
 
 export default class ResourceForm extends Component {
+  handleSubmit(event) {
+    event.preventDefault();
+ 
+    // get field contents
+    let description = this.refs.description.value.trim();
+    let quantity = this.refs.quantity.value.trim();
+    let category = this.refs.category.value.trim();
+ 
+    // insert new resource
+    try{
+      Resources.insert({
+        description: description,
+        quantity: quantity.toString(),
+        category: category.toString(),
+        createdAt: new Date(),
+      });
+    }
+    catch(err) {
+      console.log(err.message);
+    }
+ 
+    // clear form
+    this.refs.description.value = null;
+    this.refs.quantity.value = null;
+    this.refs.category.value = "placeholder";
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit.bind(this)}>
         <div className="row columns">
           <h2>Recurso Nuevo</h2>
         </div>
@@ -17,6 +45,7 @@ export default class ResourceForm extends Component {
               <div className="small-9 columns">
                 <input type="text" 
                        id="description" 
+                       ref="description"
                        placeholder="Ingresar una descripción">
                 </input>
               </div>
@@ -28,6 +57,8 @@ export default class ResourceForm extends Component {
               <div className="small-9 columns">
                 <input type="number" 
                        id="quantity" 
+                       ref="quantity" 
+                       placeholder="Ingresa la cantidad"
                        min="0" 
                        step="1">
                 </input>
@@ -38,10 +69,14 @@ export default class ResourceForm extends Component {
                 <label for="category" className="text-right middle">Categoría:</label>
               </div>
               <div className="small-9 columns">
-                <select id="category" defaultValue="placeholder">
+                <select id="category"
+                        ref="category"
+                        defaultValue="placeholder">
                   <option value="placeholder" disabled>Categorías</option>
                   {Object.keys(Constants.resource_categories).map((value, index) => (
-                    <option value="{Constants.resource_categories[value]}">{Constants.resource_categories[value]}</option>
+                    <option key={index} value={value}>
+                      {Constants.resource_categories[value]}
+                    </option>
                   ))}
                 </select>
               </div>
