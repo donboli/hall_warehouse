638de8ba3150bcbabe3a952ad0e76b824e6c2bbe
Juan Becker <wolli.becker@gmail.com>
2016-05-11 00:22:36 -0500

    create schema for resources collection

--- imports/api/resources.js
+++ imports/api/resources.js
@@ -1,3 +1,28 @@
 import { Mongo } from 'meteor/mongo';
+import Constants from '../lib/constants.js';
  
-export default Resources = new Mongo.Collection('resources');
+var Schemas = {};
+
+Schemas.Resource = new SimpleSchema({
+  description: {
+    type: String,
+    label: "Descripción",
+    max: 200
+  },
+  quantity: {
+    type: Number,
+    label: "Cantidad"
+  },
+  category: {
+    type: Number,
+    label: "Categoría",
+    allowedValues: Object.keys(Constants.resource_categories)
+                         .map((key) => { return parseInt(key); }),
+  }
+});
+
+const Resources = new Mongo.Collection('resources');
+Resources.attachSchema(Schemas.Resource);
+
+
+export default Resources;
