beb063630f7391ede018416f5ac4dabc56e6f4bd
Juan Becker <wolli.becker@gmail.com>
2016-05-29 12:48:23 -0500

    add datepicker to resources search form

--- imports/ui/resources/ResourcesSearchForm.jsx
+++ imports/ui/resources/ResourcesSearchForm.jsx
@@ -1,14 +1,32 @@
 import { Session } from 'meteor/session';
 import React, { Component } from 'react';
 import Constants from '../../lib/constants.js';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+
 
 export default class ResourcesSearchForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
       descriptionSearchValue: Session.get("descriptionSearchValue") || "",
-      categorySearchValue: Session.get("categorySearchValue") || "placeholder"
+      categorySearchValue: Session.get("categorySearchValue") || "placeholder",
+      date: moment(),
+      dateSwitch: false
     };
+  }
+
+  handleDateChange(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  handleDateSwitchChange() {
+    this.setState({
+      dateSwitch: !this.state.dateSwitch
+    });
   }
 
   setSearchValue(event) {
@@ -43,6 +61,29 @@ export default class ResourcesSearchForm extends Component {
               ref="descriptionField"
               value={this.state.descriptionSearchValue}
             />
+          </div>
+        </div>
+        <div className="row">
+          <div className="columns small-2">
+            <label for="yes-no" className="text-right middle">Fecha:</label>
+          </div>
+          <div className="columns small-6">
+            <DatePicker id="datepicker" selected={this.state.date} onChange={this.handleDateChange.bind(this)}/>
+          </div>
+          <div className="columns small-4">
+            <div className="switch large float-right">
+              <input 
+                className="switch-input" 
+                id="yes-no" 
+                type="checkbox" 
+                name="exampleSwitch" 
+                checked={this.state.dateSwitch}
+              />
+              <label className="switch-paddle" for="yes-no" onClick={this.handleDateSwitchChange.bind(this)}>
+                <span className="switch-active" aria-hidden="true">Si</span>
+                <span className="switch-inactive" aria-hidden="true">No</span>
+              </label>
+            </div>
           </div>
         </div>
         <div className="row">
